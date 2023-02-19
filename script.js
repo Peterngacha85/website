f2f7564c785f4adf0e8ffdd60f59dcb4a0d4ab1b
Peter Ngacha <96945268+Peterngacha85@users.noreply.github.com>
2023-02-19 17:16:19 +0300

    javascript  page
    for website responsive purpose

---
+++ script.js
@@ -0,0 +1,35 @@
+const Menu_a = document.getElementById('menu_icon_a');
+const Menu_b = document.getElementById('menu_icon_b');
+const Home = document.getElementById('home');
+const welcome_statement = document.getElementById('welcome_statement');
+const icon = document.querySelector('.fa-bars');
+const home = document.getElementById('Home');
+ 
+
+
+let margin_value = -500;
+const side_bar_on = () => {
+    if(margin_value == -500){
+        Show();
+    }
+    else{
+       Hide();
+    }
+
+}
+const Show = () => {
+    Home.style.marginLeft = "0px";
+        Home.style.display = "block";
+        Menu_a.style.display = "none";
+        Menu_a.style.float = "right";
+        Menu_b.style.display = "block"
+        welcome_statement.style.top = "350px"
+        margin_value = 0;
+}
+function Hide() {
+    Home.style.marginLeft = "-500px";
+    welcome_statement.style.top = "150px"
+    Menu_b.style.display = "none"
+    Menu_a.style.display = "block"
+    margin_value = -500;
+}
